c6bef9fde76a7d9163c689bf87cb8c60afae2b3e
Leo MPB <vanleotran@gmail.com>
2023-01-03 01:44:52 +0200

    fixed frontends dependency issue from backend

--- frontend/src/components/PilotList.js
+++ frontend/src/components/PilotList.js
@@ -2,17 +2,17 @@ import { useState, useEffect } from 'react';
 import Pilot from './Pilot.js'
 
 const PilotList = ({socket}) => {
-	const [pilots, setPilots] = useState([]);
+	const [data, setData] = useState([]);
 	useEffect(() => {
 		socket.on('messageResponse', (data) => {
-			setPilots(data);
-			console.log("socket data: ", data);
+			setData(data);
+			// console.log("socket data: ", data);
 		})
 	}, [socket]);
 	return (
 		<div>
-			{ pilots?.map(pilot => {
-				return <Pilot key={pilot.pilotid} pilot={pilot}/>
+			{ data?.pilots.map(pilot => {
+				{ return <Pilot key={pilot.pilotid} pilot={pilot}/> }
 			})}
 		</div>
 	)

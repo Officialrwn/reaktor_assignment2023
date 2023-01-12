2440a210ffe594b4e1178952d673e3bce1b4bc2a
Leo MPB <vanleotran@gmail.com>
2023-01-12 21:10:50 +0200

    changed nginx.conf

--- server/src/services/server.js
+++ server/src/services/server.js
@@ -9,12 +9,12 @@ app.use(cors({
 	origin: process.env.HEROKU || "http://localhost:5173"
 }));
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../client/dist')));
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(path.join(__dirname + '/../client/dist/index.html')));
 	});
-// }
+}
 
 const init = () => {
 	const PORT = process.env.PORT || 3001;
